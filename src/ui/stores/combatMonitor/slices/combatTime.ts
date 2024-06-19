42367246d28386a290349451b4acdeee91b71594
laireyx <laireyx@gmail.com>
2024-06-19 21:14:14 +0900

    refactor: seperate CombatMonitorStore into 2 slices

---
+++ src/ui/stores/combatMonitor/slices/combatTime.ts
@@ -0,0 +1,43 @@
+import { StateCreator } from 'zustand';
+
+import type { CombatTimeSlice, CombatMonitorStore } from '../types';
+
+export const createCombatTimeSlice: StateCreator<
+  CombatMonitorStore,
+  [],
+  [],
+  CombatTimeSlice
+> = (_, get) => ({
+  fightStart: -1,
+  fightEnd: -1,
+
+  pauseStart: -1,
+  totalPause: 0,
+
+  pauseDuration: () => {
+    const { status, pauseStart, totalPause } = get();
+
+    return (
+      totalPause + (status === 'inFightPaused' ? Date.now() - pauseStart : 0)
+    );
+  },
+
+  fightDuration: () => {
+    const { inFight, fightStart, fightEnd, pauseDuration } = get();
+
+    return (inFight() ? Date.now() : fightEnd) - fightStart - pauseDuration();
+  },
+
+  calcAccBuffTime: (buffKey) => {
+    const { fightBuffs } = get();
+
+    const targetBuff = fightBuffs[buffKey];
+    if (!targetBuff) return 0;
+
+    const accumulatedTime = targetBuff.accumulatedTime;
+    return (
+      accumulatedTime +
+      (targetBuff.activated ? Date.now() - targetBuff.activationTime : 0)
+    );
+  },
+});
--- src/ui/stores/combatMonitor/types.ts
+++ src/ui/stores/combatMonitor/types.ts
@@ -1,1 +1,47 @@
+import { CombatInfoLog, UiCoreLog } from '@common/types/logReader';
+import { KnownBuffs } from '@common/types/logReader/CombatInfo/buffs';
+
 export type FightStatus = 'inFight' | 'inFightPaused' | 'idle';
+
+export interface Buff {
+  activated: boolean;
+  activationTime: number;
+  accumulatedTime: number;
+}
+
+export interface CombatStatusSlice {
+  status: FightStatus;
+
+  setFightStatus: (opts: { inFight: boolean; timestamp: string }) => void;
+  inFight: () => boolean;
+
+  // Should reset these values when fight started.
+  fightBuffs: Record<string, Buff>;
+  totalDamage: number;
+  staggerCount: number;
+  qteCount: number;
+  hitCount: number;
+
+  appendBuffLog: (log: CombatInfoLog) => void;
+  appendStateMachineLog: (log: CombatInfoLog) => void;
+  appendSkillLog: (log: CombatInfoLog) => void;
+  appendHitLog: (log: CombatInfoLog) => void;
+
+  appendPlayInterfaceLog: (log: UiCoreLog) => void;
+}
+
+export interface CombatTimeSlice {
+  fightStart: number;
+  fightEnd: number;
+
+  pauseStart: number;
+  totalPause: number;
+
+  pauseDuration: () => number;
+  fightDuration: () => number;
+  calcAccBuffTime: (buffKey: keyof KnownBuffs) => number;
+}
+
+export interface CombatMonitorStore
+  extends CombatStatusSlice,
+    CombatTimeSlice {}
